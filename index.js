e87b0a74b520bff46e7ef8768414c836800af694
Andriy <andy25771@gmail.com>
2017-09-23 23:30:32 +0300

    need to fix match url

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  if(req.url.length <= 2){
-    let num = req.url.slice(1);
-    if(!isNaN(num)){
-        let index = Number(num)
-        res.end(JSON.stringify(data[index]));
-    }
+  if(req.url.length == 2){
+        let num = req.url.slice(1);
+        if(!isNaN(num)){
+            let index = Number(num)
+            res.end(JSON.stringify(data[index]));
+        }
   } else {
     res.end(JSON.stringify(data));
   }
