4b9400a17f3135ccd45c65a808c8c6e563ea4e1b
Andriy <andy25771@gmail.com>
2017-09-23 17:07:07 +0300

    added simple url parser

--- index.js
+++ index.js
@@ -6,7 +6,16 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end(JSON.stringify(data));
+  if(req.url.length <= 2){
+    let num = req.url.slice(1);
+    if(!isNaN(num)){
+        let index = Number(num)
+        res.end(JSON.stringify(data[index]));
+    }
+  } else {
+    res.end(JSON.stringify(data));
+  }
+//   console.log(req.url.length);
 });
 
 server.listen(port, () => {
